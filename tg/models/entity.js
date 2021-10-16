7fcbbec2b1f209f3e7bc9093fa6f774335c7aef2
Mizael Almeida <mzalmeida.a@gmail.com>
2021-10-16 20:58:58 -0300

    Modeled and documented required data types

---
+++ tg/models/entity.js
@@ -0,0 +1,74 @@
+/**
+ * @typedef {"mention" | "hashtag" | "cashtag" | "bot_command" | "url" | "email" | "phone_number" | "bold" | "italic" | "underline" | "strikethrough" | "code" | "pre" | "text_link" | "text_mention"} MessageEntityType
+ */
+
+/**
+ * Models a user entity
+ */
+class User {
+  /**
+   *
+   * @param {number} id - Unique identifier for this user or bot.
+   * @param {boolean} isBot - True if this user is a bot
+   * @param {string} firstName - User or bot's first name
+   * @param {string} [lastName] - (Optional) User or bot's last name
+   * @param {string} [username] - (Optional) User or bot's username
+   * @param {string} [languageCode] - (Optional) IETF language tag of the user's language
+   * @param {boolean} [canJoinGroups] - (Optional) True if the bot can be invited to groups. Returned only in getMe
+   * @param {boolean} [canReadAllGroupMessages] - (Optional) True if privacy mode is disabled for the bot. Returned only in getMe
+   * @param {boolean} [supportsInlineQueries] - (Optional) True if the bot supports inline queries. Returned Only in getMe.
+   */
+  constructor(
+    id,
+    isBot,
+    firstName,
+    lastName,
+    username,
+    languageCode,
+    canJoinGroups,
+    canReadAllGroupMessages,
+    supportsInlineQueries
+  ) {
+    this.id = id;
+    this.is_bot = isBot;
+    this.first_name = firstName;
+    if (lastName != null) this.lastName = lastName;
+    if (username != null) this.username = username;
+    if (languageCode != null) this.language_code = languageCode;
+    if (canJoinGroups != null) this.can_join_groups = canJoinGroups;
+    if (canReadAllGroupMessages != null) {
+      this.can_read_all_group_messages = canReadAllGroupMessages;
+    }
+    if (supportsInlineQueries != null) {
+      this.supports_inline_queries = supportsInlineQueries;
+    }
+  }
+}
+
+/**
+ * Models a MessageEntity object
+ */
+class MessageEntity {
+  /**
+   *
+   * @param {MessageEntityType} type - Type of the entity. Can be “mention” (@username), “hashtag” (#hashtag), “cashtag” ($USD), “bot_command” (/start@jobs_bot), “url” (https://telegram.org), “email” (do-not-reply@telegram.org), “phone_number” (+1-212-555-0123), “bold” (bold text), “italic” (italic text), “underline” (underlined text), “strikethrough” (strikethrough text), “code” (monowidth string), “pre” (monowidth block), “text_link” (for clickable text URLs), “text_mention” (for users without usernames)
+   * @param {number} offset - Offset in UTF-16 code units to the start of the entity
+   * @param {number} length - Length of the entity in UTF-16 code units
+   * @param {string} [url] - (Optional) For "text_link" only, url that will be opened after user taps on the text
+   * @param {User} [user] - (Optional) For "text_mention" only, the mentioned user
+   * @param {string} [language] - (Optional) For "pre" only, the programing language of the entity text
+   */
+  constructor(type, offset, length, url, user, language) {
+    this.type = type;
+    this.offset = offset;
+    this.length = length;
+    if (url != null) this.url = url;
+    if (user != null) this.user = user;
+    if (language != null) this.language = language;
+  }
+}
+
+module.exports = {
+  User,
+  MessageEntity,
+};
---
+++ tg/models/input.js
@@ -0,0 +1,204 @@
+const INPUT_MEDIA_PHOTO = "photo";
+const INPUT_MEDIA_VIDEO = "video";
+const INPUT_MEDIA_ANIMATION = "animation";
+const INPUT_MEDIA_AUDIO = "audio";
+const INPUT_MEDIA_DOCUMENT = "document";
+
+/**
+ * @typedef {"MarkdownV2" | "HTML"} ParseMode
+ * @typedef {import('fs').ReadStream} FsReadStream
+ */
+
+/**
+ * Base class for input media models
+ */
+class InputMedia {
+  /**
+   *
+   * @param {string} type - Type of the result. Must be one of "photo", "video", "animation", "audio" or "document"
+   * @param {string} media  - File to send. Pass a file_id to send a file that exists on the Telegram servers (recommended), pass an HTTP URL for Telegram to get a file from the Internet, or pass "attach://<file_attach_name>" to upload a new one using multipart/form-data under <file_attach_name>
+   * @param {(FsReadStream|string)} [thumb] - (Optional) Thumbnail of the file sent; can be ignored if thumbnail generation for the file is supported server-side. The thumbnail should be in JPEG format and less than 200 kB in size. A thumbnail's width and height should not exceed 320. Ignored if the file is not uploaded using multipart/form-data. Thumbnails can't be reused and can be only uploaded as a new file, so you can pass “attach://<file_attach_name>” if the thumbnail was uploaded using multipart/form-data under <file_attach_name>.
+   * @param {string} [caption] - (Optional) Caption of the media to be sent, 0-1024 characters after entities parsing
+   * @param {ParseMode} [parseMode] - (Optional) Mode for parsing entities in the media caption.
+   * @param {Array} [captionEntities] - (Optional) List of special entities that appear in the caption, which can be specified instead of parse_mode
+   */
+  constructor(type, media, thumb, caption, parseMode, captionEntities) {
+    this.type = type;
+    this.media = media;
+    if (thumb != null) this.thumb = thumb;
+    if (caption != null) this.caption = capiton;
+    if (parseMode != null) this.parse_mode = parseMode;
+    if (captionEntities != null) this.caption_entities = captionEntities;
+  }
+}
+
+/**
+ * Model for photo input
+ * @extends InputMedia
+ */
+class InputMediaPhoto extends InputMedia {
+  /**
+   *
+   * @param {string} media  - File to send. Pass a file_id to send a file that exists on the Telegram servers (recommended), pass an HTTP URL for Telegram to get a file from the Internet, or pass "attach://<file_attach_name>" to upload a new one using multipart/form-data under <file_attach_name>
+   * @param {string} [caption] - (Optional) Caption of the photo to be sent, 0-1024 characters after entities parsing
+   * @param {ParseMode} [parseMode] - (Optional) Mode for parsing entities in the photo caption.
+   * @param {Array} [captionEntities] - (Optional) List of special entities that appear in the caption, which can be specified instead of parse_mode
+   */
+  constructor(media, caption, parseMode, captionEntities) {
+    super(INPUT_MEDIA_PHOTO, media, null, caption, parseMode, captionEntities);
+  }
+}
+
+/**
+ * Model for video input
+ * @extends InputMedia
+ */
+class InputMediaVideo extends InputMedia {
+  /**
+   *
+   * @param {string} media  - File to send. Pass a file_id to send a file that exists on the Telegram servers (recommended), pass an HTTP URL for Telegram to get a file from the Internet, or pass "attach://<file_attach_name>" to upload a new one using multipart/form-data under <file_attach_name>
+   * @param {(FsReadStream|string)} [thumb] - (Optional) Thumbnail of the file sent; can be ignored if thumbnail generation for the file is supported server-side. The thumbnail should be in JPEG format and less than 200 kB in size. A thumbnail's width and height should not exceed 320. Ignored if the file is not uploaded using multipart/form-data. Thumbnails can't be reused and can be only uploaded as a new file, so you can pass “attach://<file_attach_name>” if the thumbnail was uploaded using multipart/form-data under <file_attach_name>.
+   * @param {string} [caption] - (Optional) Caption of the video to be sent, 0-1024 characters after entities parsing
+   * @param {ParseMode} [parseMode] - (Optional) Mode for parsing entities in the video caption.
+   * @param {Array} [captionEntities] - (Optional) List of special entities that appear in the caption, which can be specified instead of parse_mode
+   * @param {number} [width] - (Optional) Video width
+   * @param {number} [height] - (Optional) Video height
+   * @param {number} [duration] - (Optional) Video duration
+   * @param {boolean} [supportsStreaming] - (Optional) Pass True, if the uploaded video is suitable for streaming
+   */
+  constructor(
+    media,
+    thumb,
+    caption,
+    parseMode,
+    captionEntities,
+    width,
+    height,
+    duration,
+    supportsStreaming
+  ) {
+    super(INPUT_MEDIA_VIDEO, media, thumb, caption, parseMode, captionEntities);
+    if (width != null) this.width = width;
+    if (height != null) this.height = height;
+    if (duration != null) this.duration = duration;
+    if (supportsStreaming != null) this.supports_streaming = supportsStreaming;
+  }
+}
+
+/**
+ * Model for animation input (GIF, MP4 without audio)
+ * @extends InputMediaVideo
+ */
+class InputMediaAnimation extends InputMediaVideo {
+  /**
+   *
+   * @param {string} media  - File to send. Pass a file_id to send a file that exists on the Telegram servers (recommended), pass an HTTP URL for Telegram to get a file from the Internet, or pass "attach://<file_attach_name>" to upload a new one using multipart/form-data under <file_attach_name>
+   * @param {(FsReadStream|string)} [thumb] - (Optional) Thumbnail of the file sent; can be ignored if thumbnail generation for the file is supported server-side. The thumbnail should be in JPEG format and less than 200 kB in size. A thumbnail's width and height should not exceed 320. Ignored if the file is not uploaded using multipart/form-data. Thumbnails can't be reused and can be only uploaded as a new file, so you can pass “attach://<file_attach_name>” if the thumbnail was uploaded using multipart/form-data under <file_attach_name>.
+   * @param {string} [caption] - (Optional) Caption of the animation to be sent, 0-1024 characters after entities parsing
+   * @param {ParseMode} [parseMode] - (Optional) Mode for parsing entities in the animation caption.
+   * @param {Array} [captionEntities] - (Optional) List of special entities that appear in the caption, which can be specified instead of parse_mode
+   * @param {number} [width] - (Optional) Animation width
+   * @param {number} [height] - (Optional) Animation height
+   * @param {number} [duration] - (Optional) Animation duration
+   */
+  constructor(
+    media,
+    thumb,
+    caption,
+    parseMode,
+    captionEntities,
+    width,
+    height,
+    duration
+  ) {
+    super(
+      media,
+      thumb,
+      caption,
+      parseMode,
+      captionEntities,
+      width,
+      height,
+      duration,
+      null
+    );
+    this.type = INPUT_MEDIA_ANIMATION;
+  }
+}
+
+/**
+ * Model for audio input
+ * @Extends InputMedia
+ */
+class InputMediaAudio extends InputMedia {
+  /**
+   *
+   * @param {string} media  - File to send. Pass a file_id to send a file that exists on the Telegram servers (recommended), pass an HTTP URL for Telegram to get a file from the Internet, or pass "attach://<file_attach_name>" to upload a new one using multipart/form-data under <file_attach_name>
+   * @param {(FsReadStream|string)} [thumb] - (Optional) Thumbnail of the file sent; can be ignored if thumbnail generation for the file is supported server-side. The thumbnail should be in JPEG format and less than 200 kB in size. A thumbnail's width and height should not exceed 320. Ignored if the file is not uploaded using multipart/form-data. Thumbnails can't be reused and can be only uploaded as a new file, so you can pass “attach://<file_attach_name>” if the thumbnail was uploaded using multipart/form-data under <file_attach_name>.
+   * @param {string} [caption] - (Optional) Caption of the photo to be sent, 0-1024 characters after entities parsing
+   * @param {ParseMode} [parseMode] - (Optional) Mode for parsing entities in the photo caption.
+   * @param {Array} [captionEntities] - (Optional) List of special entities that appear in the caption, which can be specified instead of parse_mode
+   * @param {number} [duration] - (Optional) Duration of the audio in seconds
+   * @param {string} [performer] - (Optional) Performer of the audio
+   * @param {string} [title] - (Optional) Title of the audio
+   */
+  constructor(
+    media,
+    thumb,
+    caption,
+    parseMode,
+    captionEntities,
+    duration,
+    performer,
+    title
+  ) {
+    super(INPUT_MEDIA_AUDIO, media, thumb, caption, parseMode, captionEntities);
+    if (duration != null) this.duration = duration;
+    if (performer != null) this.performer = performer;
+    if (title != null) this.title = title;
+  }
+}
+
+/**
+ * Model for document input
+ * @extends InputMedia
+ */
+class InputMediaDocument extends InputMedia {
+  /**
+   *
+   * @param {string} media  - File to send. Pass a file_id to send a file that exists on the Telegram servers (recommended), pass an HTTP URL for Telegram to get a file from the Internet, or pass "attach://<file_attach_name>" to upload a new one using multipart/form-data under <file_attach_name>
+   * @param {(FsReadStream|string)} [thumb] - (Optional) Thumbnail of the file sent; can be ignored if thumbnail generation for the file is supported server-side. The thumbnail should be in JPEG format and less than 200 kB in size. A thumbnail's width and height should not exceed 320. Ignored if the file is not uploaded using multipart/form-data. Thumbnails can't be reused and can be only uploaded as a new file, so you can pass “attach://<file_attach_name>” if the thumbnail was uploaded using multipart/form-data under <file_attach_name>.
+   * @param {string} [caption] - (Optional) Caption of the document to be sent, 0-1024 characters after entities parsing
+   * @param {ParseMode} [parseMode] - (Optional) Mode for parsing entities in the document caption.
+   * @param {Array} [captionEntities] - (Optional) List of special entities that appear in the caption, which can be specified instead of parse_mode
+   * @param {boolean} [disableContentTypeDetection] - (Optional) Disables automatic server-side content type detection for files uploaded using multipart/form-data. Always true, if the document is sent as part of an album.
+   */
+  constructor(
+    media,
+    thumb,
+    caption,
+    parseMode,
+    captionEntities,
+    disableContentTypeDetection
+  ) {
+    super(
+      INPUT_MEDIA_DOCUMENT,
+      media,
+      thumb,
+      caption,
+      parseMode,
+      captionEntities
+    );
+    if (disableContentTypeDetection != null) {
+      this.disable_content_type_detection = disableContentTypeDetection;
+    }
+  }
+}
+
+module.exports = {
+  InputMediaPhoto,
+  InputMediaVideo,
+  InputMediaAnimation,
+  InputMediaAudio,
+  InputMediaDocument,
+};
