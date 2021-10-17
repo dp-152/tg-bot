4836638e64b288bb70640bef42606eb8f3edecfd
Mizael Almeida <mzalmeida.a@gmail.com>
2021-10-17 16:21:04 -0300

    Implemented sorting by date

--- core/fs/fetch-content.js
+++ core/fs/fetch-content.js
@@ -1,5 +1,24 @@
 const fs = require("fs/promises");
+const path = require("path");
 
 const { options } = require("../../util/config");
 
-exports.dirContentsPromise = fs.readdir(options.loadPath);
+const dirContentsPromise = fs.readdir(options.loadPath);
+
+exports.filesPromise = dirContentsPromise
+  .then(async res => {
+    const promises = res.map(async el => {
+      return {
+        name: el,
+        data: await fs.stat(path.join(options.loadPath, el)),
+      };
+    });
+    const fileList = await Promise.all(promises);
+    return fileList.sort(
+      (a, b) => a.data.mtime.getTime() - b.data.mtime.getTime()
+    );
+  })
+  .catch(err => {
+    console.log(err);
+    debugger;
+  });
