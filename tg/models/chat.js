134372ad664008df21d54b06b109af2f0f329714
Mizael Almeida <mzalmeida.a@gmail.com>
2021-10-18 07:04:07 -0300

    Added type definitions

--- tg/models/chat.js
+++ tg/models/chat.js
@@ -2,6 +2,8 @@
  * @typedef {"MarkdownV2" | "HTML"} ParseMode
  * @typedef {import('fs').ReadStream} FsReadStream
  * @typedef { TgChatSendMessageModel | TgChatSendPhotoModel | TgChatSendDocumentModel | TgChatSendVideoModel | TgChatSendAnimationModel | TgChatSendMediaGroupModel } TgChatModel
+ * @typedef {import('../models/entity.js').MessageEntity} MessageEntity
+ * @typedef {import('../models/input.js').InputMedia} InputMedia
  */
 
 /**
@@ -27,7 +29,7 @@ class TgChatSendModel extends TgChatModel {
    * @param {(string|number)} chatID - Unique identifier for the target chat or username of the target channel (in the format @channelusername)
    * @param {string} text - Text of the message to be sent, 1-4096 characters after entities parsing
    * @param {ParseMode} [parseMode] - (Optional) Mode for parsing entities in the message text
-   * @param {Array} [entities] - (Optional) A JSON-serialized list of special entities that appear in message text, which can be specified instead of parse_mode
+   * @param {Array<MessageEntity>} [entities] - (Optional) A JSON-serialized list of special entities that appear in message text, which can be specified instead of parse_mode
    * @param {boolean} [disableWebPagePreview]  - (Optional) Disables link previews for links in this message
    * @param {boolean} [disableNotification] - (Optional) Sends the message silently. Users will receive a notification with no sound.
    * @param {number} [replyToMessageID] - (Optional) If the message is a reply, ID of the original message
@@ -73,7 +75,7 @@ class TgChatSendMessageModel extends TgChatSendModel {
    * @param {(string|number)} chatID - Unique identifier for the target chat or username of the target channel (in the format @channelusername)
    * @param {string} text - Text of the message to be sent, 1-4096 characters after entities parsing
    * @param {ParseMode} [parseMode] - (Optional) Mode for parsing entities in the message text
-   * @param {Array} [entities] - (Optional) A JSON-serialized list of special entities that appear in message text, which can be specified instead of parse_mode
+   * @param {Array<MessageEntity>} [entities] - (Optional) A JSON-serialized list of special entities that appear in message text, which can be specified instead of parse_mode
    * @param {boolean} [disableWebPagePreview]  - (Optional) Disables link previews for links in this message
    * @param {boolean} [disableNotification] - (Optional) Sends the message silently. Users will receive a notification with no sound.
    * @param {number} [replyToMessageID] - (Optional) If the message is a reply, ID of the original message
@@ -115,7 +117,7 @@ class TgChatSendMediaModel extends TgChatSendModel {
    * @param {(string|number)} chatID - Unique identifier for the target chat or username of the target channel (in the format @channelusername)
    * @param {string} [caption] - (Optional) Media caption, 0-1024 characters after entities parsing
    * @param {ParseMode} [parseMode] - (Optional) Mode for parsing entities in the message text
-   * @param {Array} [captionEntities] - (Optional) A JSON-serialized list of special entities that appear in message text, which can be specified instead of parse_mode
+   * @param {Array<MessageEntity>} [captionEntities] - (Optional) A JSON-serialized list of special entities that appear in message text, which can be specified instead of parse_mode
    * @param {boolean} [disableNotification] - (Optional) Sends the message silently. Users will receive a notification with no sound.
    * @param {number} [replyToMessageID] - (Optional) If the message is a reply, ID of the original message
    * @param {boolean} [allowSendingWithoutReply] - (Optional) Pass True, if the message should be sent even if the specified replied-to message is not found
@@ -163,7 +165,7 @@ class TgChatSendPhotoModel extends TgChatSendMediaModel {
    * @param {(FsReadStream|string)} photo - Photo to send. Pass a file_id as String to send a photo that exists on the Telegram servers (recommended), pass an HTTP URL as a String for Telegram to get a photo from the Internet, or upload a new photo using multipart/form-data. The photo must be at most 10 MB in size. The photo's width and height must not exceed 10000 in total. Width and height ratio must be at most 20
    * @param {string} [caption] - (Optional) Photo caption (may also be used when resending photos by file_id), 0-1024 characters after entities parsing
    * @param {ParseMode} [parseMode] - (Optional) Mode for parsing entities in the message text
-   * @param {Array} [captionEntities] - (Optional) A JSON-serialized list of special entities that appear in message text, which can be specified instead of parse_mode
+   * @param {Array<MessageEntity>} [captionEntities] - (Optional) A JSON-serialized list of special entities that appear in message text, which can be specified instead of parse_mode
    * @param {boolean} [disableNotification] - (Optional) Sends the message silently. Users will receive a notification with no sound.
    * @param {number} [replyToMessageID] - (Optional) If the message is a reply, ID of the original message
    * @param {boolean} [allowSendingWithoutReply] - (Optional) Pass True, if the message should be sent even if the specified replied-to message is not found
@@ -207,7 +209,7 @@ class TgChatSendDocumentModel extends TgChatSendMediaModel {
    * @param {(FsReadStream|string)} [thumb] - (Optional) Thumbnail of the file sent; can be ignored if thumbnail generation for the file is supported server-side. The thumbnail should be in JPEG format and less than 200 kB in size. A thumbnail's width and height should not exceed 320. Ignored if the file is not uploaded using multipart/form-data. Thumbnails can't be reused and can be only uploaded as a new file, so you can pass “attach://<file_attach_name>” if the thumbnail was uploaded using multipart/form-data under <file_attach_name>.
    * @param {string} [caption] - (Optional) Document caption (may also be used when resending documents by file_id), 0-1024 characters after entities parsing
    * @param {ParseMode} [parseMode] - (Optional) Mode for parsing entities in the message text
-   * @param {Array} [captionEntities] - (Optional) A JSON-serialized list of special entities that appear in message text, which can be specified instead of parse_mode
+   * @param {Array<MessageEntity>} [captionEntities] - (Optional) A JSON-serialized list of special entities that appear in message text, which can be specified instead of parse_mode
    * @param {boolean} [disableContentTypeDetection] - (Optional) Disables automatic server-side content type detection for files uploaded using multipart/form-data
    * @param {boolean} [disableNotification] - (Optional) Sends the message silently. Users will receive a notification with no sound.
    * @param {number} [replyToMessageID] - (Optional) If the message is a reply, ID of the original message
@@ -260,7 +262,7 @@ class TgChatSendVideoModel extends TgChatSendMediaModel {
    * @param {(FsReadStream|string)} [thumb] - (Optional) Thumbnail of the file sent; can be ignored if thumbnail generation for the file is supported server-side. The thumbnail should be in JPEG format and less than 200 kB in size. A thumbnail's width and height should not exceed 320. Ignored if the file is not uploaded using multipart/form-data. Thumbnails can't be reused and can be only uploaded as a new file, so you can pass “attach://<file_attach_name>” if the thumbnail was uploaded using multipart/form-data under <file_attach_name>.
    * @param {string} [caption] - (Optional) Video caption (may also be used when resending videos by file_id), 0-1024 characters after entities parsing
    * @param {ParseMode} [parseMode] - (Optional) Mode for parsing entities in the message text
-   * @param {Array} [captionEntities] - (Optional) A JSON-serialized list of special entities that appear in message text, which can be specified instead of parse_mode
+   * @param {Array<MessageEntity>} [captionEntities] - (Optional) A JSON-serialized list of special entities that appear in message text, which can be specified instead of parse_mode
    * @param {boolean} [disableNotification] - (Optional) Sends the message silently. Users will receive a notification with no sound.
    * @param {number} [replyToMessageID] - (Optional) If the message is a reply, ID of the original message
    * @param {boolean} [allowSendingWithoutReply] - (Optional) Pass True, if the message should be sent even if the specified replied-to message is not found
@@ -315,7 +317,7 @@ class TgChatSendAnimationModel extends TgChatSendVideoModel {
    * @param {(FsReadStream|string)} [thumb] - (Optional) Thumbnail of the file sent; can be ignored if thumbnail generation for the file is supported server-side. The thumbnail should be in JPEG format and less than 200 kB in size. A thumbnail's width and height should not exceed 320. Ignored if the file is not uploaded using multipart/form-data. Thumbnails can't be reused and can be only uploaded as a new file, so you can pass “attach://<file_attach_name>” if the thumbnail was uploaded using multipart/form-data under <file_attach_name>.
    * @param {string} [caption] - (Optional) Animation caption (may also be used when resending animations by file_id), 0-1024 characters after entities parsing
    * @param {ParseMode} [parseMode] - (Optional) Mode for parsing entities in the message text
-   * @param {Array} [captionEntities] - (Optional) A JSON-serialized list of special entities that appear in message text, which can be specified instead of parse_mode
+   * @param {Array<MessageEntity>} [captionEntities] - (Optional) A JSON-serialized list of special entities that appear in message text, which can be specified instead of parse_mode
    * @param {boolean} [disableNotification] - (Optional) Sends the message silently. Users will receive a notification with no sound.
    * @param {number} [replyToMessageID] - (Optional) If the message is a reply, ID of the original message
    * @param {boolean} [allowSendingWithoutReply] - (Optional) Pass True, if the message should be sent even if the specified replied-to message is not found
@@ -364,7 +366,7 @@ class TgChatSendMediaGroupModel extends TgChatSendMediaModel {
   /**
    *
    * @param {(string|number)} chatID - Unique identifier for the target chat or username of the target channel (in the format @channelusername)
-   * @param {Array} mediaArr - A JSON-serialized array describing messages to be sent, must include 2-10 items
+   * @param {Array<InputMedia>} mediaArr - A JSON-serialized array describing messages to be sent, must include 2-10 items
    * @param {boolean} disableNotification - (Optional) Sends messages silently. Users will recieve a notification with no sound.
    * @param {number} replyToMessageID - (Optional) If the messages are a reply, ID of the original message
    * @param {boolean} allowSendingWithoutReply - (Optional) Pass True if the message should be sent even if the specified replied-to message is not found
