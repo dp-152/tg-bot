5c5e6356966918d027df20f8f9d2eb0ae1c0d8a0
Mizael Almeida <mzalmeida.a@gmail.com>
2021-10-18 07:52:38 -0300

    Moved worker code into functions

--- core/assembler/parse-files.js
+++ core/assembler/parse-files.js
@@ -1,79 +1,94 @@
 const path = require("path");
 
-const { filesPromise } = require("../fs/fetch-content");
 const types = require("../models/types");
 
-exports.parsedFilesPromise = filesPromise
-  .then(files => {
-    const skippedFileNames = [];
-    const parsedList = [];
-    files.forEach(currFile => {
-      // Return if file is in skip list
-      if (skippedFileNames.indexOf(currFile.name) >= 0) return;
+/**
+ * Parses a list of files
+ * Will append file extension, media type, thumbnail (if available) and caption (if available)
+ * @param {Promise} fileList - Promise containing file list to be parsed
+ * @return {Promise} - Promise containing file list with parsed data
+ */
+function parseFileList(fileList) {
+  return fileList
+    .then(files => {
+      const skippedFileNames = [];
+      const parsedList = [];
 
-      // Grab the extension
-      currFile.ext = path.extname(currFile.name);
-      // Find if file extension is known
-      currFile.type = types.knownMedia.find(
-        el => el.exts.indexOf(currFile.ext) >= 0
-      ).type;
-      // Unknown extensions are passed as documents
-      if (!currFile.type) currFile.type = types.TYPE_MEDIA_DOC;
-      // If a video or GIF file has a "_animation" suffix,
-      // it is passed as an animation instead
-      else if (
-        (currFile.type === types.TYPE_MEDIA_VIDEO ||
-          currFile.ext === types.TYPE_EXT_GIF) &&
-        currFile.name.match(
-          new RegExp("^.*" + types.TYPE_SUFFIX_ANIM + "\\..*$", "g")
-        )
-      ) {
-        currFile.type = types.TYPE_MEDIA_ANIM;
-      }
+      files.forEach(currFile => {
+        // Return if file is in skip list
+        if (skippedFileNames.indexOf(currFile.name) >= 0) return;
 
-      // Look for a thumb image only for document, video
-      // and animation types
-      if (
-        [
-          types.TYPE_MEDIA_DOC,
-          types.TYPE_MEDIA_VIDEO,
-          types.TYPE_MEDIA_ANIM,
-        ].indexOf(currFile.type) >= 0
-      ) {
-        const thumbFileName = currFile.name + types.TYPE_SUFFIX_THUMB;
-        const thumbFile = files.find(el => {
-          return (
-            el.name === thumbFileName + types.TYPE_EXT_JPG ||
-            el.name === thumbFileName + types.TYPE_EXT_JPEG
-          );
-        });
-        if (thumbFile) {
-          skippedFileNames.push(thumbFile.name);
-          currFile.thumbFile = thumbFile;
+        // Grab the extension
+        currFile.ext = path.extname(currFile.name);
+        // Find if file extension is known
+        currFile.type = types.knownMedia.find(
+          el => el.exts.indexOf(currFile.ext) >= 0
+        ).type;
+        // Unknown extensions are passed as documents
+        if (!currFile.type) currFile.type = types.TYPE_MEDIA_DOC;
+        // If a video or GIF file has a "_animation" suffix,
+        // it is passed as an animation instead
+        else if (
+          (currFile.type === types.TYPE_MEDIA_VIDEO ||
+            currFile.ext === types.TYPE_EXT_GIF) &&
+          currFile.name.match(
+            new RegExp("^.*" + types.TYPE_SUFFIX_ANIM + "\\..*$", "g")
+          )
+        ) {
+          currFile.type = types.TYPE_MEDIA_ANIM;
         }
-      }
 
-      // Look for a caption file for every non-text file
-      if (currFile.type !== types.TYPE_MEDIA_TEXT) {
-        const captionFileName = currFile.name + types.TYPE_SUFFIX_CAPTION;
-        const captionFile = files.find(el => {
-          return (
-            el.name === captionFileName + types.TYPE_EXT_TXT ||
-            el.name === captionFileName + types.TYPE_EXT_MD ||
-            el.name === captionFileName + types.TYPE_EXT_HTM ||
-            el.name === captionFileName + types.TYPE_EXT_HTML
-          );
-        });
-        if (captionFile) {
-          skippedFileNames.push(captionFile.name);
-          currFile.captionFile = captionFile;
+        // Look for a thumb image only for document, video
+        // and animation types
+        if (
+          [
+            types.TYPE_MEDIA_DOC,
+            types.TYPE_MEDIA_VIDEO,
+            types.TYPE_MEDIA_ANIM,
+          ].indexOf(currFile.type) >= 0
+        ) {
+          const thumbFileName = currFile.name + types.TYPE_SUFFIX_THUMB;
+          const thumbFile = files.find(el => {
+            return (
+              el.name === thumbFileName + types.TYPE_EXT_JPG ||
+              el.name === thumbFileName + types.TYPE_EXT_JPEG
+            );
+          });
+          if (thumbFile) {
+            skippedFileNames.push(thumbFile.name);
+            currFile.thumbFile = thumbFile;
+          }
         }
-      }
-      parsedList.push(currFile);
+
+        // Look for a caption file for every non-text file
+        // TODO: Condense this double iteration into one?
+        if (currFile.type !== types.TYPE_MEDIA_TEXT) {
+          const captionFileName = currFile.name + types.TYPE_SUFFIX_CAPTION;
+          const captionFile = files.find(el => {
+            return (
+              el.name === captionFileName + types.TYPE_EXT_TXT ||
+              el.name === captionFileName + types.TYPE_EXT_MD ||
+              el.name === captionFileName + types.TYPE_EXT_HTM ||
+              el.name === captionFileName + types.TYPE_EXT_HTML
+            );
+          });
+          if (captionFile) {
+            skippedFileNames.push(captionFile.name);
+            currFile.captionFile = captionFile;
+          }
+        }
+
+        // Push current file to the return list
+        parsedList.push(currFile);
+      });
+      return parsedList;
+    })
+    .catch(err => {
+      console.log(err);
+      debugger;
     });
-    return parsedList;
-  })
-  .catch(err => {
-    console.log(err);
-    debugger;
-  });
+}
+
+module.exports = {
+  parseFileList,
+};
--- core/fs/fetch-content.js
+++ core/fs/fetch-content.js
@@ -3,23 +3,33 @@ const path = require("path");
 
 const { options } = require("../../util/config");
 
-const dirContentsPromise = fs.readdir(options.loadPath);
-
-exports.filesPromise = dirContentsPromise
-  .then(async res => {
-    res = res.sort();
-    const promises = res.map(async el => {
-      return {
-        name: el,
-        stat: await fs.stat(path.join(options.loadPath, el)),
-      };
+/**
+ * Fetches all files in the content directory set in config.json
+ * @return {Promise} - Promise that resolves to the sorted file list of the content directory
+ */
+function fetchSortedFileList() {
+  const dirContentsPromise = fs.readdir(options.loadPath);
+  const fileListPromise = dirContentsPromise
+    .then(async res => {
+      res = res.sort();
+      const promises = res.map(async el => {
+        return {
+          name: el,
+          stat: await fs.stat(path.join(options.loadPath, el)),
+        };
+      });
+      const fileList = await Promise.all(promises);
+      return fileList.sort(
+        (a, b) => a.stat.mtime.getTime() - b.stat.mtime.getTime()
+      );
+    })
+    .catch(err => {
+      console.log(err);
+      debugger;
     });
-    const fileList = await Promise.all(promises);
-    return fileList.sort(
-      (a, b) => a.stat.mtime.getTime() - b.stat.mtime.getTime()
-    );
-  })
-  .catch(err => {
-    console.log(err);
-    debugger;
-  });
+  return fileListPromise;
+}
+
+module.exports = {
+  fetchSortedFileList,
+};
