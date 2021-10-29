054ff0887e848e27f2d8c120c35f33b4f68c184d
Mizael Almeida <mzalmeida.a@gmail.com>
2021-10-29 14:22:48 -0300

    Implemented file exclusion routine

---
+++ core/fs/move-sent.js
@@ -0,0 +1,32 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const { options } = require("../../util/config");
+
+async function moveFile(filePath) {
+  console.log(
+    `Moving file ${path.basename(filePath)} to ${options.historyPath}`
+  );
+  return fs.rename(
+    filePath,
+    path.resolve(options.historyPath, path.basename(filePath))
+  );
+}
+
+async function moveSentFiles(fileList) {
+  const promises = [];
+  for (file of fileList) {
+    promises.push(moveFile(file.path));
+    if (file.thumbFile) {
+      promises.push(moveFile(file.thumbFile.path));
+    }
+    if (file.captionFile) {
+      promises.push(moveFile(file.captionFile.path));
+    }
+  }
+  await Promise.all(promises);
+}
+
+module.exports = {
+  moveSentFiles,
+};
