1a8109576be136c5b0a226c314187dca18e19b94
Mizael Almeida <mzalmeida.a@gmail.com>
2021-10-29 11:00:36 -0300

    Updated sender routine behavior

--- core/routine/sender.js
+++ core/routine/sender.js
@@ -1,19 +1,23 @@
 const { options } = require("../../util/config");
+const { initFillQueue } = require("./queue-fill");
 const { pullN } = require("../queue/queue");
-const { send } = require("./tg/interface/send");
+const { send } = require("../../tg/interface/send");
 
+async function sendJob() {
+  const list = pullN(options.sendAtOnce);
+  for (msg of list) {
+    console.log(msg);
+    await send(msg.data);
+    await new Promise(res => {
+      setTimeout(res, 5000);
+    });
+  }
+}
 
-function initSendJob() {
-  setInterval(async () => {
-    const list = pullN(options.sendAtOnce);
-    for (msg of list) {
-      console.log(msg);
-      await send(msg.data);
-      await new Promise(res => {
-        setTimeout(res, 5000);
-      });
-    }
-  }, options.sendEvery * 60 * 1000);
+async function initSendJob() {
+  await initFillQueue();
+  sendJob();
+  setInterval(sendJob, options.sendEvery * 60 * 1000);
 }
 
 module.exports = {
