ef29decc50b0415967abd0fe8f86909cf84a3fe1
Mizael Almeida <mzalmeida.a@gmail.com>
2021-10-29 11:01:07 -0300

    Added exclude list for files that should be moved out of the queue

--- core/queue/queue.js
+++ core/queue/queue.js
@@ -1,14 +1,28 @@
 const queue = [];
+const exclude = [];
 
-function addToQueue(...messages) {
+function addToQueue(messages) {
   queue.push(...messages);
+}
+
+function addToExclude(messages) {
+  exclude.push(...messages);
 }
 
 function pullN(n) {
   return queue.splice(0, n);
 }
 
+function pullExclude() {
+  return exclude.splice(0, exclude.length);
+}
+
 module.exports = {
   addToQueue,
   pullN,
+  addToExclude,
+  pullExclude,
+  get currentQueue() {
+    return queue.map(el => el.name);
+  },
 };
